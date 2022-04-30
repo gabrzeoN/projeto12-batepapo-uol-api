6d5822c595a9c8bf13d51164d82a1bd33a7fcd0f
Gabriel Cari <g.cari@hotmail.com>
2022-04-30 15:08:07 -0300

    Feat: Getting name on POST/participants, validating, returning status 422 in case of validation failures.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,61 @@
-import cors from "cors";
-import express from "express";
 import { MongoClient } from "mongodb";
-import chalk from "chalk";
+import express from "express";
 import dotenv from "dotenv";
+import chalk from "chalk";
+import cors from "cors";
+import joi from "joi";
 
+// Server configurations
 dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.listen(process.env.PORT, () => console.log(chalk.bold.green(`Server online on port ${process.env.PORT}!`)));
 
+// Database configurations
 let db = null;
-const mongoClient = MongoClient(process.env.MONGO_URI);
-mongoClient.connect();
-db = mongoClient.db(process.env.DATABASE)
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+mongoClient.connect()
+.then(() => {
+    db = mongoClient.db(process.env.DATABASE);
+    console.log(chalk.bold.green("Connected to database!"));
+})
+.catch((error) => console.log(chalk.bold.red("Could't connet to database!"), error));
+        
+// Joi schemas
+const userSchema = joi.object({
+    name: joi.string().required()
+});
 
-// const participants = {
-//    name: 'João',
-//    lastStatus: 12313123
-// }
+app.post("/participants", async (req, res) => {
+    const {name} = req.body;
 
-// const messages = {
-//     from: 'João',
-//     to: 'Todos',
-//     text: 'oi galera',
-//     type: 'message',
-//     time: '20:04:37'
-// }
+    const validation = userSchema.validate({name}, {abortEarly: false});
+    if(validation.error){
+        console.log(validation.error.details.map(detail => detail.message)); // TODO: erase me
+        return res.status(422).send("name deve ser string não vazio!");
+    }
+
+    try{
+        await db.collection("participants").insertOne({name});
+        res.sendStatus(201);
+    }catch(e){
+        console.log("Error on POST /participants", e);
+        res.sendStatus(500);
+    }
+});
+
+
+
+    // const participants = {
+    //    name: 'João',
+    //    lastStatus: 12313123
+    // }
+    
+    // const messages = {
+    //     from: 'João',
+    //     to: 'Todos',
+    //     text: 'oi galera',
+    //     type: 'message',
+    //     time: '20:04:37'
+    // }
